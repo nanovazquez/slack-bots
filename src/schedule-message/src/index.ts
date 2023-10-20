993946353b381d44e22c9a3b5057749a7aefae7d
nanovazquez <marianodvazquez@gmail.com>
2023-10-20 16:36:51 -0300

    Add bolt to the repo with basic interaction

--- src/schedule-message/src/index.ts
+++ src/schedule-message/src/index.ts
@@ -1,9 +1,44 @@
 import "dotenv/config";
+import { App } from "@slack/bolt";
 
-function getSalute() {
-  return process.env.HELLO;
-}
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
 
-console.log("hi", getSalute());
+app.message("hello", async ({ message, say }) => {
+  if (!message.subtype || message.subtype === "bot_message") {
+    await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hey there <@${message.user}>!`,
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+            },
+            action_id: "button_click",
+          },
+        },
+      ],
+      text: `Hey there <@${message.user}>!`,
+    });
+  }
+});
 
-export { getSalute };
+app.action("button_click", async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("⚡️ Bolt app is running!");
+})();
